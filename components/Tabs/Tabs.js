30929049d25e93efd0ea7d9f32ea7d4cf1c23dbc
Patrick <patrick.schwindt14@gmail.com>
2019-01-31 16:13:49 -0700

    Update Again

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -11,7 +11,7 @@ class TabLink {
     this.itemElement = this.element.querySelector(`.tabs-item[data-tab='${element.dataset.tab}']`);
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem(this.itemElement);
+    this.tabItem = new TabItem(this.tabItem);
     
     // Add a click event listener on this instance, calling the select method on click
     element.addEventListener('click', () => this.select());
@@ -20,22 +20,20 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    const links = document.querySelector('.tabs-link');
+    const links = document.querySelectorAll('.tabs-link');
     const linksSelected = document.querySelector('.tabs-links-selected');
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    //Array.from(links).forEach(this.element =>);
-
-    // for (let i = 0; i < links.length; i ++){
-    //   console.log(links[i]);
-    // }
+    Array.from(links).forEach(linker => linker.classList.remove('.tabs-link-removed'));
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element = this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
 
-    console.log(this.links);
+    this.tabItem.select();
+
+    //console.log(links[i]);
 
   }
 }
